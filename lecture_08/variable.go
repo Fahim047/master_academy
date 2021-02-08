f2725282cbbb2bd0146015afb820617b57296292
Fahim047 <fahimulislam58@gmail.com>
2021-02-08 09:05:28 +0600

    lecture_08 has been added

---
+++ lecture_08/variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//single line comment
+var name string
+name= "Fahimul Islam"
+var name2, city string = "Rian", "Dhaka"
+fmt.Println(name, city, name2)
+/*
+multiple
+line
+comment
+*/
+var age int = 21
+var cgpa float32 = 3.75
+fmt.Println(age, cgpa)
+
+}
