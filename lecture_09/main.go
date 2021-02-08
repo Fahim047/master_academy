dd421f7bcb7120b7be782488a4f660d63e1c3372
Fahim047 <fahimulislam58@gmail.com>
2021-02-08 09:14:27 +0600

    lecture_09 has been added

---
+++ lecture_09/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	//var isFound bool
+	/*
+	fmt.Println(isFound)
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	*/
+	var char rune ='a'
+	fmt.Println(char)
+	fmt.Printf("%c", char)
+
+
+}
