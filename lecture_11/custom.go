6c90bd01d0004606c62f3aee0440ef1693100b7a
Fahim047 <fahimulislam58@gmail.com>
2021-02-13 22:08:26 +0600

    structure and custom data types

---
+++ lecture_11/custom.go
@@ -0,0 +1,3 @@
+package main
+
+type height float32 //custom type
---
+++ lecture_11/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+/*type Book struct {
+		Titile string
+		Author string
+		ISBN string
+		Price float32
+		Pages int
+}*/
+
+func main() {
+/*
+	var b1 Book
+	b1.Titile="AN INTRODUCTION TO PROGRAMING IN GO"
+	b1.Author="CALEB DOXY"
+	b1.ISBN="978-1478355823"
+	b1.Price=9.99
+	b1.Pages=165
+
+	fmt.Println(b1)
+	fmt.Println(b1.Titile)
+	fmt.Println(b1.Author)
+	fmt.Println(b1.ISBN)
+	fmt.Println(b1.Price)
+	fmt.Println(b1.Pages)
+*/
+/*
+	b1 := struct{
+		Titile string
+		Author string
+		ISBN string
+		Price float32
+		Pages int
+	}{
+		Titile: "AN INTRODUCTION TO PROGRAMING IN GO",
+		Author: "CALEB DOXY",
+		ISBN: "978-1478355823",
+		Price: 9.99,
+		Pages: 165,
+	}
+	fmt.Println(b1)
+	fmt.Println(b1.Titile)
+	fmt.Println(b1.Author)
+	fmt.Println(b1.ISBN)
+	fmt.Println(b1.Price)
+	fmt.Println(b1.Pages)
+*/
+	var h1 height // custom type
+	fmt.Println(h1)
+}
