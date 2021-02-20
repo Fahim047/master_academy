11e502c7db436ebcd28fe10dfbe739441b75f9d8
Fahim047 <fahimulislam58@gmail.com>
2021-02-20 13:45:21 +0600

    Passing address as parameter

--- lecture_13/function.go
+++ lecture_13/function.go
@@ -24,10 +24,17 @@ func add4(x int, y int) (z int){
 	return
 }
 */
+/*
 func rectangle(l int, b int) (area int, parameter int) {
 	parameter = 2 * (l + b)
 	area = l * b
 	return // Return statement without specify variable name
+}
+*/
+func update(a *int, t *string) {
+	*a = *a + 5      // defrencing pointer address
+	*t = *t + " Fahad" // defrencing pointer address
+	return
 }
 
 func main(){
@@ -41,9 +48,15 @@ func main(){
 	sum4 := add(20,30)
 	fmt.Println(sum4)
 	*/
-
+	/*
 	var a, p int
 	a, p = rectangle(20, 30)
 	fmt.Println("Area:", a)
 	fmt.Println("Parameter:", p)
+	*/
+	age :=21
+	name := "Fahim"
+	update(&age, &name)
+	fmt.Println(age,name)
+
 }
